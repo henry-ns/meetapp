a61f1e558a3f7a26e181753c65cefc8fffb00114
Henrique Martins <enrque.ns@gmail.com>
2019-06-28 08:49:38 -0300

    chore: inputs validation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 routes.get('/', (_, res) => res.send('Welcome to MeetApp'));
+
+routes.post('/session', SessionController.store);
+
+routes.route('/users').post(UserController.store);
 
 export default routes;
