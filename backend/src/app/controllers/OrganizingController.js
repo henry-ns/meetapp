2e366aa9798b7ca3023b599142d4b1e050655655
Henrique Martins <enrque.ns@gmail.com>
2019-07-18 19:37:28 -0300

    chore: return file data together meetup

--- backend/src/app/controllers/OrganizingController.js
+++ backend/src/app/controllers/OrganizingController.js
@@ -1,9 +1,17 @@
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class OrganizingController {
   async index(req, res) {
     const meetups = await Meetup.findAll({
       where: { user_id: req.userId },
+      include: [
+        {
+          model: File,
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+      order: [['date', 'ASC']],
     });
 
     return res.json(meetups);
