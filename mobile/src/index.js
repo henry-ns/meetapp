6a2b205b945e7b61fd2cbfac2e04a960530d6d95
Henrique Miranda <enrque.ns@gmail.com>
2019-07-24 17:41:26 -0300

    init mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello Meetapp</Text>
+    </View>
+  );
+};
+
+export default App;
